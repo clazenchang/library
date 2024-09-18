4474c3da124cd8788a98261377a9f1a379d11618
clazenchang <clazenchang@gmail.com>
2024-09-18 17:29:55 +0800

    post image from json file

--- src/components/booksList.vue
+++ src/components/booksList.vue
@@ -1,9 +1,12 @@
 <script setup>
 import books from '@/assets/books.json'
-console.log(books)
+//console.log(books)
+const bookList = books;
+console.log(bookList[1].book)
 </script>
 
 
 <template>
     This is booksList
+    <img :src="bookList[1].picURL">
 </template>
