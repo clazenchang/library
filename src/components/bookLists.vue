a11f747133528a92f654cca41e46dfdac9a589a9
clazenchang <clazenchang@gmail.com>
2024-09-24 17:22:59 +0800

    add author Array(haven't filtered) in bookLists.vue

--- src/components/bookLists.vue
+++ src/components/bookLists.vue
@@ -1,39 +1,41 @@
 <script setup>
 import books from '@/assets/books.json'
 import bookList from '@/components/bookList.vue'
+
+var authorArr = [];
+for(let book of books){
+  authorArr.push(book.author)
+}
+console.log(authorArr)
+
+// function filteredAuthors(){
+//   var authors = 0
+// }
+
 </script>
 
 
 <template>
   <div class="row">
+
     <div class="d-none d-md-flex col-md-2 mt-4">
-      some content
+      <div class="d-flex flex-column mb-3">
+        <div class="p-2" v-for="aBook in books" :key="aBook.id">{{ aBook.author }}</div>
+
+        <!-- <div class="p-2">Flex item 2</div>
+        <div class="p-2">Flex item 3</div> -->
+      </div>
     </div>
 
     <div class="col-10">
       <div class="container mt-4">
         <div class="row justify-content-around">
 
-          <bookList v-for="book in books" :key="book.id" :book="book" />
-          <!-- <div class="card mb-3 col-12 col-md-5">
-            <div class="row g-0">
-              <div class="col-md-4">
-                <img :src="bookList[0].picURL" class="img-fluid rounded-start" alt="...">
-              </div>
-              <div class="col-md-8">
-                <div class="card-body">
-                  <h5 class="card-title">{{ bookList[0].book }}</h5>
-                  <p class="mb-0">作者:{{ bookList[0].author }}</p>
-                  <p class="mb-0">出版年:{{ bookList[0].year }}</p>
-                  <p class="mb-1">語言別:{{ bookList[0].language }}</p>
-                  <p class="card-text">This is a wider card with supporting text below as a natural...</p>
-                </div>
-              </div>
-            </div>
-          </div> -->
+          <bookList v-for="aBook in books" :key="aBook.id" :book="aBook" />
 
         </div>
       </div>
     </div>
+
   </div>
 </template>
