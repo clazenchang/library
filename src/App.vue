a259078f7a53941afb3a75c5b2c5a37c6690ab9a
clazenchang <clazenchang@gmail.com>
2024-09-24 16:15:46 +0800

    v-for and add bookLists.vue page

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,12 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
 //import HelloWorld from './components/HelloWorld.vue'
-import booksList from '@/components/booksList.vue'
+import bookLists from '@/components/bookLists.vue'
 </script>
 
 <template>
 
-<booksList />
+<bookLists />
 </template>
 
 <style scoped>
