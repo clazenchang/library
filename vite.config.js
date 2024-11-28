6e867746505eb3a84ebcd5e71e0e0ecbba0b85ed
clazenchang <clazenchang@gmail.com>
2024-11-28 14:08:17 +0800

    adding dist

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // base: '/library/',   // just for gh-pages
+  base: '/library/',   // just for gh-pages
   plugins: [
     vue(),
   ],
